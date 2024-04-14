d7688efa60a833efdec54c896a63afaaab870bd0
Gabyfelicios <167029558+Gabyfelicios@users.noreply.github.com>
2024-04-14 19:20:47 -0300

    Add files via upload
    Pilha

--- main.cpp
+++ main.cpp
@@ -1,38 +1,22 @@
-#include "pilha.h"
-#include<stdio.h>
+#include <iostream>
+#include "matematica.h"
+#include <math.h>
 
-int main(){
-    Pilha* pilha = pilha_cria();
-    int valor;
+using std::cout;
+using std::endl;
 
 
-    pilha_push(pilha, 1);
-    pilha_push(pilha, 2);
-    pilha_push(pilha, 3);
-    
-    valor = pilha_pop(pilha);
-    printf("RETIRADO = %d \n", valor);
-    
-    pilha_push(pilha, 3);
-    
-    valor = pilha_pop(pilha);
-    printf("RETIRADO = %d \n", valor);
-    
-    valor = pilha_pop(pilha);
-    printf("RETIRADO = %d \n", valor);
-    
-    valor = pilha_pop(pilha);
-    printf("RETIRADO = %d \n", valor);
-    
-    valor = pilha_pop(pilha);
-    printf("RETIRADO = %d \n", valor);
-    
-    pilha_push(pilha, 1);
-    pilha_push(pilha, 20);
-    imprime(pilha);
-    
-    
-    pilha_apagar(pilha);
+int main() {
+    float angulo;
+    std::cout<< "escreva um angulo em radianos";
+    std::cin>> angulo;
 
+    
+    float senoAngulo= seno (angulo);
+    double cosAngulo= cosseno (angulo);
+    double tanAngulo= tangente (angulo);
+    cout << "Seno: " <<senoAngulo << std::endl;
+    cout << "Cosseno: " <<cosAngulo << std::endl;
+    cout << "Tangente: " <<tanAngulo << std::endl;
     return 0;
 }
